8e1fcede6e77ea75c5ed8573a04ed0423fdb720f
jbielawski <foxandthefiddle@comcast.net>
2025-08-14 15:23:01 -0500

    Add reports and stats

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_words(string):
-    words = string.split()
-    return len(words)
+from stats import *
+import sys
 
 def count_characters(string):
     character_counts = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0}
@@ -23,14 +22,20 @@ def display_report(character_count, word_count):
     character_list = convert_to_list(character_count)
     character_list.sort(reverse=True, key=sort_by)
 
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{word_count} words found in the document\n")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("-------- Character Count -------")
     for i in range(0, len(character_list)):
-        print(f"The '{character_list[i]['character']}' character was found {character_list[i]['num']} times")
-    print("--- End report ---")
+        print(f"{character_list[i]['character']}: {character_list[i]['num']}")
+    print("============= END ===============")
 
 def main():
-    with open("books/frankenstein.txt") as f:
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    with open(sys.argv[1]) as f:
         file_contents = f.read()
 
     word_count = count_words(file_contents)
---
+++ stats.py
@@ -0,0 +1,5 @@
+
+def count_words(string):
+    words = string.split()
+    return len(words)
+        
